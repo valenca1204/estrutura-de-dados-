5e5e269cc789d886530ea32a70516669579db838
valenca1204 <pedrorsvalenca@gmail.com>
2025-02-20 19:22:46 -0300

    Criado arquivo .py

--- estrutura_de_dados_1.py
+++ estrutura_de_dados_1.py
@@ -1,6 +1,11 @@
-# estrutura-de-dados-
-
-
+#Crie um sistema de chamados, o sistema deverá permitir:
+#Cadastrar novos chamados
+#Buscar chamados por ID ou descrição
+#Remover chamados finalizados
+#Listar chamados em ordem de prioridade
+#Exibir estatísticas sobre os chamados
+#Reverter e limpar a lista de chamados 
+#Projeto deve ser inserido no Github, listado como público. 
 
 chamados = []
 
